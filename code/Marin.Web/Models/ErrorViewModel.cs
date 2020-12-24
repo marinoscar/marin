e4e4eeac5771dd3930118ce35df37b42b97b56e1
marinoscar <oscar@marin.cr>
2020-12-24 12:41:30 -0600

    Initial commit

---
+++ code/Marin.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Marin.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ code/Marin/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marin
+{
+    /// <summary>
+    /// Application entry point
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main entry point to the application
+        /// </summary>
+        /// <param name="args">Arguments</param>
+        static void Main(string[] args)
+        {
+            var arguments = new AppArgs(args);
+
+            RunAction(() =>
+            {
+                DoAction(arguments);
+
+            }, true);
+        }
+
+        /// <summary>
+        /// Executes an action on the application
+        /// </summary>
+        /// <param name="arguments"></param>
+        static void DoAction(AppArgs arguments)
+        {
+            WriteLine("Hello World");
+        }
+
+        /// <summary>
+        /// Runs the action and handles exceptions
+        /// </summary>
+        /// <param name="action">The action to execute</param>
+        public static void RunAction(Action action, bool waitForKey = false)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception exception)
+            {
+                WriteLineError(exception.ToString());
+            }
+            finally
+            {
+                if (waitForKey)
+                {
+                    WriteLineInfo("Press any key to end");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        #region Console Methods
+
+        /// <summary>
+        /// Writes an message to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="format">The string to format</param>
+        /// <param name="arg">The arguments to format the string</param>
+        public static void Write(ConsoleColor color, string format, params object[] arg)
+        {
+            var current = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(format, arg);
+            Console.ForegroundColor = current;
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="format">The string to format</param>
+        /// <param name="arg">The arguments to format the string</param>
+        public static void WriteLine(ConsoleColor color, string format, params object[] arg)
+        {
+            WriteLine(color, string.Format(format, arg));
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="format">The string to format</param>
+        /// <param name="arg">The arguments to format the string</param>
+        public static void WriteLine(string format, params object[] arg)
+        {
+            WriteLine(Console.ForegroundColor, string.Format(format, arg));
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="message">The string to format</param>
+        public static void WriteLine(ConsoleColor color, string message)
+        {
+            var current = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = current;
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="message">The string to format</param>
+        public static void WriteLine(string message)
+        {
+            WriteLine(Console.ForegroundColor, message);
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="format">The string to format</param>
+        /// <param name="arg">The arguments to format the string</param>
+        public static void WriteLineInfo(string format, params object[] arg)
+        {
+            WriteLine(format, arg);
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="message">The string to format</param>
+        public static void WriteLineInfo(string message)
+        {
+            WriteLine(message);
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="format">The string to format</param>
+        /// <param name="arg">The arguments to format the string</param>
+        public static void WriteLineWarning(string format, params object[] arg)
+        {
+            WriteLine(ConsoleColor.Yellow, format, arg);
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="message">The string to format</param>
+        public static void WriteLineWarning(string message)
+        {
+            WriteLine(ConsoleColor.Yellow, message);
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="format">The string to format</param>
+        /// <param name="arg">The arguments to format the string</param>
+        public static void WriteLineError(string format, params object[] arg)
+        {
+            WriteLine(ConsoleColor.Red, format, arg);
+        }
+
+        /// <summary>
+        /// Writes a new line to the console
+        /// </summary>
+        /// <param name="color">The forground color of the message</param>
+        /// <param name="message">The string to format</param>
+        public static void WriteLineError(string message)
+        {
+            WriteLine(ConsoleColor.Red, message);
+        }
+
+        #endregion
+    }
+}
