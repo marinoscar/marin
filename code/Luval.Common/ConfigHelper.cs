bae84b8dafd74e44f177f351e7567543e5165b93
Oscar Marin <oscar@marin.cr>
2021-11-28 19:16:54 -0600

    improved code readability

--- code/Luval.Common/ConfigHelper.cs
+++ code/Luval.Common/ConfigHelper.cs
@@ -10,7 +10,9 @@ namespace Luval.Common
     public static class ConfigHelper
     {
         private static Dictionary<Type, IConfiguration> _configs = new Dictionary<Type, IConfiguration>() { { typeof(ConfigManagerWrapper), new ConfigManagerWrapper() } };
-        private static ObjectCache<string, IConfiguration> _cache = new ObjectCache<string, IConfiguration>();
+        private static ObjectCache<string, IConfiguration> _cacheConfig = new ObjectCache<string, IConfiguration>();
+        private static ObjectCache<string, string> _cacheValue = new ObjectCache<string, string>();
+
         public static void RegisterProvider(IConfiguration configuration)
         {
             if (configuration == null) throw new ArgumentNullException("configuration", "value cannot be null");
@@ -18,29 +20,51 @@ namespace Luval.Common
             _configs.Add(configuration.GetType(), configuration);
         }
 
+        /// <summary>
+        /// Gets a configuration value from one of the registered providers
+        /// </summary>
+        /// <param name="key">Configuration key</param>
+        /// <param name="cacheValue">Indicates if the value should be cached in memory once found to minimize the number of requests to the config provider</param>
+        /// <returns>Configuration value</returns>
+        public static string Get(string key, bool cacheValue)
+        {
+            if (cacheValue) return _cacheValue.Get(key, () => { return Get(key); });
+            return Get(key);
+        }
+
+        /// <summary>
+        /// Gets a configuration value from one of the registered providers
+        /// </summary>
+        /// <param name="key">Configuration key</param>
+        /// <returns>Configuration value</returns>
         public static string Get(string key)
         {
             if (string.IsNullOrWhiteSpace(key)) throw new ArgumentNullException("Parameter cannot be null or empty");
-            return _cache.Get(key, () => {
+            return _cacheConfig.Get(key, () => {
 
-                var exs = new List<Exception>();
-                foreach (var item in _configs)
-                {
-                    string value = null;
-                    try
-                    {
-                        value = item.Value[key];
-                        if (!string.IsNullOrWhiteSpace(value)) return item.Value;
-                    }
-                    catch (Exception ex)
-                    {
-                        exs.Add(new InvalidOperationException("Unable to get value for type {0}".Format(item.Key), ex));
-                    }
-                }
-                if (exs.Any()) throw new ArgumentException("Unable to locate the setting\n {0}".Format(string.Join("\n", exs.Select(i => i.ToString()))));
-                return new EmptyConfiguration();
+                return GetValue(key);
 
             })[key];
+        }
+
+        private static IConfiguration GetValue(string key)
+        {
+            var exs = new List<Exception>();
+            foreach (var item in _configs)
+            {
+                string value = null;
+                try
+                {
+                    value = item.Value[key];
+                    if (!string.IsNullOrWhiteSpace(value)) return item.Value;
+                }
+                catch (Exception ex)
+                {
+                    exs.Add(new InvalidOperationException("Unable to get value for type {0}".Format(item.Key), ex));
+                }
+            }
+            if (exs.Any()) throw new ArgumentException("Unable to locate the setting\n {0}".Format(string.Join("\n", exs.Select(i => i.ToString()))));
+            return new EmptyConfiguration();
         }
     }
 
