64dbdc19ae2fd4c31e2b2c3fbef49c20bda353b9
Oscar Marin <oscar@marin.cr>
2021-11-29 19:04:05 -0600

    improvements

--- code/Luval.UrlShortner.Web/Areas/Shortner/Controllers/ShortnerController.cs
+++ code/Luval.UrlShortner.Web/Areas/Shortner/Controllers/ShortnerController.cs
@@ -1,19 +1,54 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Luval.Data.Extensions;
+using Luval.UrlShortner.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Luval.Common;
 
 namespace Luval.UrlShortner.Web.Areas.Shortner.Controllers
 {
-    [Area("Shortner"), Authorize]
+    [Area("Shortner"), AllowAnonymous]
     public class ShortnerController : Controller
     {
+        public ShortnerController(IUrlShortnerRepository urlShortnerRepository)
+        {
+            Repository = urlShortnerRepository;
+        }
+
+        public IUrlShortnerRepository Repository { get; private set; }
+
         [HttpGet, Route("Shortner")]
         public IActionResult Index(string id)
         {
             return View();
+        }
+
+        [HttpPost, Route("Shortner/Create")]
+        public async Task<IActionResult> Create(ShortName item, CancellationToken cancellationToken)
+        {
+            if (item == null) return Json(new ShortnerViewModel() { Success = false, Message = "Invalid payload" });
+            if (await Repository.GetByIdAsync(item.Id, cancellationToken) != null)
+                return Json(new ShortnerViewModel() { Success = false, IsDuplicate = true, Message = "Short Code {0} already exists".Format(item.Id) });
+            ShortName result = null;
+            try
+            {
+                result = await Repository.AddUrlAsync(item.Id, item.OriginalUri, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ShortnerViewModel() { Success = false, IsDuplicate = false, Message = "Short Code {0} failed with error: {1}".Format(item.Id, ex.Message) });
+            }
+            return Json(new ShortnerViewModel()
+            {
+                Success = true,
+                IsDuplicate = false,
+                Message = "New short url created succesfully",
+                Url = "{0}://{1}/r/{2}".Format(Request.Scheme, Request.Host, result.Id)
+            }); ;
         }
     }
 }
--- code/Luval.UrlShortner.Web/resources/lib/luval.shortner/shortner.js
+++ code/Luval.UrlShortner.Web/resources/lib/luval.shortner/shortner.js
@@ -1,5 +1,74 @@
 ﻿var shortner = {
     init: function() {
-        return true;
+        $('#show-code').hide();
+        $('#UrlId').focus();
+        $('#create-button').on('click', function () {
+            shortner.createItem();
+        });
+        $('#create-new').on('click', function () {
+            location.reload();
+        });
+    },
+    createItem: function () {
+        var payload = shortner.getObject();
+        if (!shortner.isValidUrl(payload.OriginalUri)) {
+            shortner.setAlertMessage('Please enter a valid url', 'danger');
+            return;
+        }
+        $.ajax({
+            type: 'post',
+            url: '/Shortner/Create',
+            data: payload,
+            success: function (data, status, jqXHR) {
+                if (jqXHR.status == "200") {
+                    if (data.isSuccess == true)
+                        shortner.onSuccess(data);
+                    else
+                        shortner.setAlertMessage(data.message, 'danger');
+                    return;
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status == "200") {
+                    return;
+                }
+                alert('Failed to save the post ' + jqXHR.status + ' ' + jqXHR.statusText);
+            },
+            dataType: "json"
+        });
+    },
+    onSuccess: function (data) {
+        shortner.setAlertMessage(data.message, 'success');
+        $('#create-code').hide();
+        $('#show-code').show();
+    },
+    validate: function (payload) {
+        return shortner.isValidUrl(payload.OriginalUri);
+    },
+    setAlertMessage: function (message, type) {
+        var banner = $('#alertbanner');
+        var textEl = $('#alertmessage');
+        banner.removeClass('alert-success');
+        banner.removeClass('alert-warning');
+        banner.removeClass('alert-danger');
+        banner.removeClass('alert-primary');
+        banner.addClass('alert-' + type);
+        textEl.html(message);
+        banner.show().delay(30000).fadeOut();
+    },
+    getObject: function () {
+        return {
+            Id: $('#IdText').val(),
+            OriginalUri: $('#UrlId').val()
+        };
+    },
+    isValidUrl: function (value) {
+        let url;
+        try {
+            url = new URL(value);
+        } catch (_) {
+            return false;
+        }
+        return url.protocol === "http:" || url.protocol === "https:";
     }
 }
