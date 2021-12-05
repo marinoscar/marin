4ed54b9f6871f2c6a620802fffe0c38d92a72e3f
Oscar Marin <oscar@marin.cr>
2021-12-04 19:31:41 -0600

    added a common web library

---
+++ code/Luval.Web.Common/LuvalConfigurationOptions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.Web.Common
+{
+    public class LuvalConfigurationOptions : IPostConfigureOptions<StaticFileOptions>
+    {
+
+        public LuvalConfigurationOptions(IWebHostEnvironment environment)
+        {
+            HostEnvironment = environment;
+        }
+
+        public IWebHostEnvironment HostEnvironment { get; private set; }
+
+        public virtual void PostConfigure(string name, StaticFileOptions options)
+        {
+
+            options.ContentTypeProvider ??= new FileExtensionContentTypeProvider();
+
+            if (options.FileProvider == null && HostEnvironment.WebRootFileProvider == null)
+            {
+                throw new InvalidOperationException("Missing FileProvider.");
+            }
+
+            options.FileProvider = options.FileProvider ?? HostEnvironment.WebRootFileProvider;
+
+
+            // Add our provider
+            var filesProvider = new ManifestEmbeddedFileProvider(GetType().Assembly, "resources");
+            options.FileProvider = new CompositeFileProvider(options.FileProvider, filesProvider);
+        }
+    }
+}
---
+++ code/Luval.Web.Common/resources/lib/luval.common/luval.common.js
@@ -0,0 +1,5 @@
+﻿var utils = {
+    init: function () {
+        return true;
+    }
+}
