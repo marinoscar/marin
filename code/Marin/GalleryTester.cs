4d8de18f155a914eca6c2ad1e56961e7d61618a5
Oscar Marin <oscar@marin.cr>
2021-12-06 21:28:13 -0600

    added a sink application

---
+++ code/Marin/GalleryTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Luval.Media.Gallery;
+using Luval.Media.Gallery.OneDrive;
+
+namespace Marin
+{
+    public class GalleryTester
+    {
+
+        public AuthenticationOptions GetAuthenticationOptions()
+        {
+            return null;
+        }
+
+    }
+}
