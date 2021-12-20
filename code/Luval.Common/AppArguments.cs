534c1a98bdbc21d76a51c7b0f304152d8e09a33d
Oscar Marin <oscar@marin.cr>
2021-12-19 21:10:01 -0600

    Added the ability to pass console arguments as a config value

---
+++ code/Luval.Common/AppArguments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luval.Common
+{
+    /// <summary>
+    /// Provides an abstraction to handle common application argument switches
+    /// </summary>
+    public class AppArguments
+    {
+        private List<string> _args;
+
+        /// <summary>
+        /// Creates an instance of the class
+        /// </summary>
+        /// <param name="args">Collection of arguments</param>
+        public AppArguments(IEnumerable<string> args)
+        {
+            _args = new List<string>(args);
+        }
+
+        /// <summary>
+        /// Gets the value for the switch in the argument collection
+        /// </summary>
+        /// <param name="name">The name of the switch</param>
+        /// <returns>The switch value if present, otherwise null</returns>
+        public string this[string name]
+        {
+            get
+            {
+                var idx = _args.IndexOf(name);
+                if (idx == (_args.Count - 1)) return null;
+                return _args[idx + 1];
+            }
+        }
+
+        /// <summary>
+        /// Indicates if the switch exists in the argument collection
+        /// </summary>
+        /// <param name="name">The name of the switch</param>
+        /// <returns>True if the switch name is on the colleciton, otherwise false</returns>
+        public bool ContainsSwitch(string name)
+        {
+            return _args.Contains(name);
+        }
+    }
+}
---
+++ code/Luval.Common/AppArgumentsConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luval.Common
+{
+    public class AppArgumentsConfig : IConfiguration
+    {
+        public AppArgumentsConfig(IEnumerable<string> args)
+        {
+            AppArguments = new AppArguments(args);
+        }
+
+        public string this[string key] { get {
+                if (AppArguments.ContainsSwitch("/" + key))
+                    return AppArguments["/" + key];
+                else
+                    return null;
+            } set => throw new NotImplementedException(); }
+
+        protected virtual AppArguments AppArguments { get; private set; }
+
+        public IEnumerable<IConfigurationSection> GetChildren()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChangeToken GetReloadToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfigurationSection GetSection(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
