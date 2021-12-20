0046a681cbc821f062c35e1d029035150b304847
Oscar Marin <oscar@marin.cr>
2021-12-19 21:25:39 -0600

    added config options

--- code/Luval.Media.Gallery.Web/GalleryServiceCollectionExtensions.cs
+++ code/Luval.Media.Gallery.Web/GalleryServiceCollectionExtensions.cs
@@ -9,13 +9,13 @@ namespace Luval.Media.Gallery.Web
 {
     public static class GalleryServiceCollectionExtensions
     {
-        public static void AddGallery(this IServiceCollection services, string sqlConnectionString)
+        public static void AddGallery(this IServiceCollection services, string sqlConnectionString, OAuthAuthoizationOptions authoizationOptions)
         {
             services.ConfigureOptions(typeof(GalleryConfigurationOptions));
             services.AddTransient<IMediaGalleryRepository>((sp) => {
                 return new MediaGalleryRepository();
             });
-
+            services.AddSingleton<OAuthAuthoizationOptions>(authoizationOptions);
         }
     }
 }
