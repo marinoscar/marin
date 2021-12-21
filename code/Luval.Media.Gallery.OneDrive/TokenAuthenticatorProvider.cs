95d1fb13654dcb040e776ec6c6db82d6ba6e5503
Oscar Marin <oscar@marin.cr>
2021-12-20 23:09:00 -0600

    added class to process authentication

---
+++ code/Luval.Media.Gallery.OneDrive/TokenAuthenticatorProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.Media.Gallery.OneDrive
+{
+    public class TokenAuthenticatorProvider : IAuthenticationProvider
+    {
+        private string _token;
+        public TokenAuthenticatorProvider(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token)) throw new ArgumentNullException("token");
+            _token = token;
+        }
+
+        public Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            return Task.Run(() => {
+                request.Headers.Add("Authorization", string.Format("Bearer {0}", _token));
+            });
+        }
+    }
+}
